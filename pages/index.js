8cc5e9ef63d470c543064936bc1e220ba969b5b0
austinkarren <nerrak87@gmail.com>
2021-05-06 20:47:27 -0600

    Span tags around events for red text

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
             {/* CARD 1 */}
             <div className={globalStyles.card}>
               <div className={globalStyles.textContainer}>
-                <p className={globalStyles.p}>EVENTS | MAY 05 2021</p>
+                <p className={globalStyles.p}><span className={globalStyles.eventP}>EVENTS</span> | MAY 05 2021</p>
                 <h1 className={globalStyles.h1}>LEARN BASIC BING PREP TODAY</h1>
                 <button className={globalStyles.button}>REGISTER</button>
               </div>
@@ -56,7 +56,7 @@ export default function Home() {
             {/* CARD 2 */}
             <div className={globalStyles.card}>
               <div className={globalStyles.textContainer}>
-                <p className={globalStyles.p}>EVENTS | MAY 05 2021</p>
+                <p className={globalStyles.p}><span className={globalStyles.eventP}>EVENTS</span> | MAY 05 2021</p>
                 <h1 className={globalStyles.h1}>COOKING WITH <br></br>CHEF MING</h1>
                 <button className={globalStyles.button}>REGISTER</button>
               </div>
@@ -72,7 +72,7 @@ export default function Home() {
             {/* CARD 3 */}
             <div className={globalStyles.card}>
               <div className={globalStyles.textContainer}>
-                <p className={globalStyles.p}>EVENTS | MAY 05 2021</p>
+                <p className={globalStyles.p}><span className={globalStyles.eventP}>EVENTS</span> | MAY 05 2021</p>
                 <h1 className={globalStyles.h1}>COOKING WITH<br></br> OUR FOUNDERS</h1>
                 <button className={globalStyles.button}>REGISTER</button>
               </div>
@@ -101,8 +101,8 @@ export default function Home() {
                   ></img>
                 </div>  
                 <div className={globalStyles.bTextContainer}>
-                  <p className={globalStyles.p}>EVENTS | MAY 05 2021</p>
-                  <h1 className={globalStyles.h1}>THIS CHEF'S ON HIS OWN MISSION</h1>
+                  <p className={globalStyles.p}><span className={globalStyles.eventP}>EVENTS</span> | MAY 05 2021</p>
+                  <h1 className={globalStyles.h1}>CHEF ON A MISSION</h1>
                   <button className={globalStyles.button}>READ ARTICLE</button>
                 </div>
               </div>
@@ -117,7 +117,7 @@ export default function Home() {
                   ></img>
                 </div>  
                 <div className={globalStyles.bTextContainer}>
-                    <p className={globalStyles.p}>EVENTS | MAY 05 2021</p>
+                    <p className={globalStyles.p}><span className={globalStyles.eventP}>EVENTS</span> | MAY 05 2021</p>
                     <h1 className={globalStyles.h1}>LEARN BASIC BING PREP TODAY</h1>
                     <button className={globalStyles.button}>READ RECIPE</button>
                 </div>
@@ -133,7 +133,7 @@ export default function Home() {
                     ></img>
                 </div>  
                 <div className={globalStyles.bTextContainer}>
-                    <p className={globalStyles.p}>EVENTS | MAY 05 2021</p>
+                    <p className={globalStyles.p}><span className={globalStyles.eventP}>EVENTS</span> | MAY 05 2021</p>
                     <h1 className={globalStyles.h1}>HIGH QUALITY TRADITION</h1>
                     <button className={globalStyles.button}>READ ARTICLE</button>
                 </div>
